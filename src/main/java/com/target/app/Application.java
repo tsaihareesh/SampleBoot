2053bd50e6324dd536c239ebf9ceac4761ce644e
tsaihareesh <tsaihareesh@gmail.com>
2016-11-30 14:53:22 +0530

    Update Application.java

--- src/main/java/com/target/app/Application.java
+++ src/main/java/com/target/app/Application.java
@@ -12,7 +12,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans pred by Spring Boot:");
+        System.out.println("Let's in git inspect the beans pred by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
